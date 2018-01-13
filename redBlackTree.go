0b34bc8fc0bb7095402b8af467bc41d715d14d41
sdlaovip@gmail.com <sdlaovip@gmail.com>
2018-01-13 07:40:56 -0800

    rbt

---
+++ redBlackTree.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("red-black-tree")
+}
+
+type node struct {
+	d int
+	c bool
+	l *node
+	r *node
+	p *node
+}
+
+func insert() {
+
+}
