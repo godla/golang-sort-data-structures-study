deee5a5650846b420c1b4d8ec4713cbc865c2c96
sdlaovip@gmail.com <sdlaovip@gmail.com>
2018-01-20 00:28:41 -0800

    skip list

---
+++ SkipList.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("skip list")
+	data := []int{1, 2, 3, 4, 5, 6, 7, 8}
+}
+
+func insert() {
+
+}
+
+func delete() {
+
+}
+
+func search() {
+
+}
