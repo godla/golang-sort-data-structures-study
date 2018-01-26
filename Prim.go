617fd49d51c8afa488a24c56b262283ffe65fcd3
sdlaovip@gmail.com <sdlaovip@gmail.com>
2018-01-26 00:10:03 -0800

    prim

--- Prim.go
+++ Prim.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+var MAX_SIZE = 1000
+
 func main() {
 	fmt.Println("Prim")
+}
+
+type Graph struct {
+	vexs   [MAX_SIZE]string        //定点集合
+	vexnum int                     //定点数量
+	edgnum int                     //边数量
+	matri  [MAX_SIZE][MAX_SIZE]int //邻接矩阵
+}
+
+func init() {
+
+}
+
+func prim() {
+
 }
